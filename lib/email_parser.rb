79e6ef7fd213e7909bb679d430a513d895aacce9
jessicashannon <jessdahling@gmail.com>
2015-08-04 15:46:26 -0400

    wooo regular expressions

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -1,3 +1,19 @@
-# Build a class EmailParser that accepts a string of unformatted 
-# emails. The parse method on the class should separate them into
-# unique email addresses. The delimiters to support are commas (',')
+# # Build a class EmailParser that accepts a 
+# string of unformatted 
+# # emails. The parse method on the class should
+#  separate them into
+# # unique email addresses. The delimiters to support 
+# are commas (',')
+
+class EmailParser
+  attr_accessor :emails
+
+  def initialize(email_list)
+    @emails = email_list
+  end
+
+  def parse
+    @emails.split(/\s|\,\s/).uniq
+  end
+
+end
